9150aa4e7825e42e40d5371390cf1fc99a0a2898
Keira Stavast <114077660+RoseThorn78@users.noreply.github.com>
2023-03-02 20:45:21 +1030

    Which Marvel Character Are You?

---
+++ main (1).py
@@ -0,0 +1,24 @@
+print("Marvel/Guardians of the Galaxy Movie Character Creator")
+print()
+spiderMan = input("Do you like 'hanging around'?: ")
+if spiderMan == "Yes":
+  print("Woohoo, you're Spider Man!")
+else:
+  print("Then you're definitely not Spider Man!")
+  print()
+  groot = input("Can you say anything other than your name?: ")
+  if groot == "No":
+     print("Congratulations, you and Groot must be siblings, you're so similar!")
+  else:
+     print("Then you can't possibly be Groot!")
+     print()
+     thanos = input("Do you often wish you could make half the world disappear?: ")
+     if thanos == "Yes":
+      print("Then todays your lucky day! You're Thanos!")
+     else:
+        print("Ahh, that's unfortunate. Making people vanish sounded fun...")
+        ironMan = input("Do you enjoy building and inventing stuff?: ")
+        if ironMan == "Yes":
+             print("Then you're definitely Iron Man!")
+        else:
+             print("Nope, you absolutely can't be Iron Man!")
