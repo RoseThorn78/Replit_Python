3a9049b9dc4f8df7e3fb2dfb2abda856e0927ace
Keira Stavast <114077660+RoseThorn78@users.noreply.github.com>
2024-04-02 22:57:22 +1030

    Updated version!
    Previous bugs resolved, and new code uploaded! 😁

---
+++ Favourite Christmas Movie Quiz/main (5).py
@@ -0,0 +1,24 @@
+print("Favourite Christmas Movie Quiz!")
+print()
+movie = input("What is your favourite Christmas movie?")
+print()
+if movie == "elf" or movie == "Elf":
+  print("Really? Elf is the best!")
+  print()
+  faveCharacter = input("Who is your favourite character?")
+  print()
+  if faveCharacter == "Buddy" or faveCharacter == "buddy":
+    print("Yup, Buddy's the most hilarious Christmas movie character ever!")
+  else:
+    print("That character is okay, I guess...")
+elif movie == "How the grinch stole christmas" or movie == "how the grinch stole christmas" or movie == "How The Grinch Stole Christmas" or movie == "How the Grinch stole Christmas":
+  print("The absolute greatest Christmas movie ever created!")
+  print()
+  movieVersion = input("Do you prefer the live action, or the new cartoon version?")
+  print()
+  if movieVersion == "Live Action" or movieVersion == "live action" or movieVersion == "Live action":
+    print("Live Action TV is awesome, especially when you're watching the Grinch!")
+  else:
+     print("That's great and all...")
+else:
+  print("Well, everyone's got their own tastes in movies..")   
